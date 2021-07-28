ea80455c7bf2cd5ba144be9996e822f4d09fbc93
ylyxa <ylyxa000@gmail.com>
2021-07-28 14:03:54 +0300

    Added GUI #5

--- Parser.py
+++ Parser.py
@@ -1,12 +1,23 @@
 import ply.yacc as yacc
 from Lexer import tokens, find_column
 import Translate
+from tkinter import *
+from tkinter import filedialog as fd, ttk
+
 options = {
     'strings-exp': False,
     'produce-models': False
 }
 
 funcs = set()
+
+
+def choose_input():
+    infile.set(fd.askopenfilename())
+
+
+def choose_output():
+    outfile.set(fd.asksaveasfilename(initialfile='out.ref', defaultextension='.ref'))
 
 
 class Error(Exception):
@@ -161,14 +172,47 @@ def p_error(p):
 parser = yacc.yacc()
 
 
-if __name__ == '__main__':
-    with open('tests/test_parentheses_inside_const.smt2', 'r') as input_file:
+def convert():
+    with open(infile.get(), 'r') as input_file:
         data = input_file.read()
 
     result = parser.parse(data)
     if not result:
         raise GenericError('Parsing failed!')
+    result, preds = Translate.translateToCNF(list(result))
 
-    result,preds= Translate.translateToCNF(list(result))
+    with open(outfile.get(), 'w') as output_file:
+        pass
+
     print(result)
     print(preds)
+
+
+if __name__ == '__main__':
+
+    root = Tk()
+    root.title('SMT to REF')
+
+    mainframe = ttk.Frame(root, padding='3 3 12 12')
+    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+    infile = StringVar()
+    outfile = StringVar()
+
+    infile_entry = ttk.Entry(mainframe, width=100, textvariable=infile)
+    infile_entry.grid(column=2, row=1, sticky=(W, E), padx=(10, 10))
+    open_ = ttk.Button(mainframe, text='Browse', command=choose_input)
+    open_.grid(column=3, row=1, sticky=E)
+
+    outfile_entry = ttk.Entry(mainframe, width=100, textvariable=outfile)
+    outfile_entry.grid(column=2, row=2, sticky=(W, E), padx=(10, 10))
+    save = ttk.Button(mainframe, text='Browse', command=choose_output)
+    save.grid(column=3, row=2, sticky=E)
+
+    ttk.Label(mainframe, text='Input:').grid(column=1, row=1, sticky=E)
+    ttk.Label(mainframe, text='Output:').grid(column=1, row=2, sticky=E)
+
+    make = ttk.Button(mainframe, text='Convert', command=convert)
+    make.grid(column=4, row=2, sticky=E, padx=(10, 0))
+
+    root.mainloop()
